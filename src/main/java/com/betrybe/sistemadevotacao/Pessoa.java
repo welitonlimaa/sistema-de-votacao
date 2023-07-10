64f4ebb191d5828ab405db9f7cafde068cc2a130
welitonlimaa <welitonlima159@gmail.com>
2023-07-10 14:34:43 -0300

    req 1

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-//  ola
+/**
+ * Classe abstrata que representa uma pessoa. Serve como base para implementação de classes
+ * específicas de pessoas.
+ */
+public abstract class Pessoa {
+
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
+
