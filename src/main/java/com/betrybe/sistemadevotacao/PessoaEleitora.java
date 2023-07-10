7528f03947aa2059b3ff7c935a596ea6bfb24e2c
welitonlimaa <welitonlima159@gmail.com>
2023-07-10 17:11:09 -0300

    req 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,56 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Classe que representa uma pessoa eleitora. Herda da classe Pessoa.
+ */
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Construtor da classe PessoaEleitora.
+   *
+   * @param nome O nome da pessoa eleitora.
+   * @param cpf  O CPF da pessoa eleitora.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Obtém o nome identificador da pessoa eleitora.
+   *
+   * @return O nome.
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  /**
+   * Define o nome identificador da pessoa eleitora.
+   *
+   * @param nome O nome identificador para voto.
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * Obtém o CPF da pessoa eleitora.
+   *
+   * @return O CPF da pessoa eleitora.
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Define o CPF da pessoa eleitora.
+   *
+   * @param cpf CPF da pessoa eleitora.
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
